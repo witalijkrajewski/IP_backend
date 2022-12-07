4782af6ba4aea6e61feb30b6359035a6871da67c
Witalij Krajewski <115834531+witalijkrajewski@users.noreply.github.com>
2022-12-07 04:48:14 +0300

    IP-3/feature.views and urls created

--- back/products/urls.py
+++ back/products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all-hookahs/', views.ProductsList.as_view()),
+    path('all-products/', views.ProductsList.as_view()),
     path('products/<slug:category_slug>/<product_slug>/', views.ProductDetail.as_view()),
 
 ]
